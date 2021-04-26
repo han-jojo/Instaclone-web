9116ea232ef0eb313abbf7f8b114631b648a3fa5
Jiho Han <mlpzaq123456@naver.com>
2021-04-26 13:57:43 +0900

    11.1 ~ 11.2 Header part

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,11 +2,10 @@ import { useHistory } from "react-router";
 import { logUserOut } from "../apollo";
 
 function Home() {
-  const history = useHistory();
   return (
     <div>
       <h1>Hello world!</h1>
-      <button onClick={() => logUserOut(history)}>Log out now!</button>
+      <button onClick={() => logUserOut()}>Log out now!</button>
     </div>
   );
 }
